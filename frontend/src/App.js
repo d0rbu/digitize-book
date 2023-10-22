89281fb6e85d2c4aede17af1dc6e54bf11b509dd
KtwoNn <khangnguyent.2002@gmail.com>
2023-10-21 21:17:18 -0500

    finished components for flashcards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Upload from './components/Upload';
+import ToC from './components/ToC';
+import flashcards from './components/flashcards';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact Component={Upload} />
+        <Route path="/toc" exact Component={ToC} />
+        <Route path="/flashcards" exact Component={flashcards} />
+
       </Routes>
     </BrowserRouter>
   );
